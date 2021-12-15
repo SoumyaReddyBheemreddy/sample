b9037c6b38304444dd5233cce6fcfacd13858558
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2021-12-15 11:41:25 +0530

    second commit

---
+++ src/main/java/com/example/Sum.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Sum {
+    private int num1;
+    private int num2;
+    Sum(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    public int sumOfTwoNumbers(){
+        return num1+num2;
+    }
+}
