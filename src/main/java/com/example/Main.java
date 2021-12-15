417e9d9128b5651486a2d82f225b3bf9820b41cd
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2021-12-15 11:25:13 +0530

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        int number1 =10;
+        int number2 = 20;
+        int sum = number1+number2;
+
+        System.out.println(sum);
+    }
+}
